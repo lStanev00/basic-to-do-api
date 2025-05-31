5c16c6ac987bace3ede7f88b5e4bdc877ac19703
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-31 10:52:25 +0300

    todos collection feeding

--- src/collections/todos.ts
+++ src/collections/todos.ts
@@ -1,6 +1,35 @@
-import { Item } from "../types/types.js"
+import generateItem from "../helpers/generateItem.ts";
+import { Item, Task } from "../types/types.ts"
 
 const todos: Item[] = [];
 
+const task1: Task = {
+    name: `Simple In-Memory API for Managing Items`,
+    task: `create Simple In-Memory API for Managing Items`,
+    finished: false
+}
+
+const task2: Task = {
+    name: `Simple React To-Do List with Filtering`,
+    task: `create Simple React To-Do List with Filtering`,
+    finished: false
+}
+
+const task3: Task = {
+    name: `API with Basic Validation and Error Handling`,
+    task: `create API with Basic Validation and Error Handling`,
+    finished: false
+}
+
+const task4: Task = {
+    name: `Simple React Data Fetching and Display`,
+    task: `create React Data Fetching and Display`,
+    finished: false
+}
+
+todos.push(generateItem(task1, todos));
+todos.push(generateItem(task2, todos));
+todos.push(generateItem(task3, todos));
+todos.push(generateItem(task4, todos));
 
 export default todos;
