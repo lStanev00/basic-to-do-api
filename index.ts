de4a92d8f3751ab5b15fe832409aad1f342f8906
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-31 16:25:27 +0300

    cors setup

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './src/router.ts';
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors())
 app.use(express.json());
 
 app.use(`/`, router)
