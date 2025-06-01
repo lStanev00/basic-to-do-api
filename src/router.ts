c63807161ff09b5281827d6a81c10c50596fe76d
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-06-01 09:39:30 +0300

    implementation of the controllers with correct  requests

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getItems, postItem } from "./controllers/items.ts";
+import { changeFinish, deleteItem, getItems, postItem } from "./controllers/items.ts";
 
 const router = Router();
 
 router.get(`/items`, getItems);
 router.post(`/items`, postItem);
+router.patch(`/items`, changeFinish)
+router.delete(`/items`, deleteItem)
 
 export default router
