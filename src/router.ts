e0cb4a5496d6f7c52786b234f87b1c95c49fe5e4
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-06-10 00:23:46 +0300

    generic route

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
-import { changeFinish, deleteItem, getItems, postItem, updateItem } from "./controllers/items.ts";
+import { changeFinish, deleteItem, getItems, postItem, updateItem, getItem } from "./controllers/items.ts";
 
 const router = Router();
 
 router.get(`/items`, getItems);
-router.get(`/items/:id`, getItems);
+router.get(`/items/:id`, getItem);
 router.post(`/items`, postItem);
 router.patch(`/items`, changeFinish)
 router.put(`/items`, updateItem);
-router.delete(`/items`, deleteItem)
+router.delete(`/items`, deleteItem);
+router.use(`/`, (_,res) => {
+    res.status(500).send(`Generic server error`)
+})
 
 export default router
