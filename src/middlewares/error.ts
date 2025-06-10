0e8534f04a2b2c2fa3177878dc1e08806d7ce179
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-06-10 09:26:41 +0300

    import as type

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import type { Request, Response, NextFunction } from "express";
 export default async function errorMiddleware(err: Error, req: Request, res: Response, next: NextFunction): Promise<void> {
     console.warn(err.stack);
     res.status(500).send(`Generic Server Error!`);
